b8ea022b2d93e0689b76fd83f5146a7a0277d4c9
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-25 05:38:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def dictionary
+dictionary = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter
+end
